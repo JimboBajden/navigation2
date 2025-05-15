2ee0b3165b4709be335723c13e2e02826f0678e6
4TPprzemysławCurzydł <jeba ydw>
2025-05-15 11:01:08 +0200

    mesershmit

--- navigation2/MVVM/Pages/NewPage1.xaml.cs
+++ navigation2/MVVM/Pages/NewPage1.xaml.cs
@@ -18,27 +18,13 @@ public partial class NewPage1 : ContentPage
         census.Add(new Person() { name = "Fruli", surname = "Frula"});
         census.Add(new Person() { name = "Tung tung tung tung", surname = "Sahur"});
         census.Add(new Person() { name = "Tripi", surname = "Tropi"});
-        census.Add(new Person() { name = "Odin din din din din dun", surname = "ma dun"});
+        census.Add(new Person() { name = "Odin din din din din dun", surname = "Odin din din din din din Madun"});
 
         
         CensusDisplay.BindingContext = census;
         CensusDisplay.ItemsSource = census;
 	}
-
-    private async void CensusDisplay_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        /*
-        Uri url = new Uri("https://crazycattle3d.github.io/");
-        await Browser.Default.OpenAsync(url, BrowserLaunchMode.SystemPreferred);
-        */
-
-        if (CensusDisplay.SelectedItem is Person selectedPerson)
-        {
-            Navigation.PushAsync(new NewPage2(selectedPerson, census, CensusDisplay));
-            
-        }
-        CensusDisplay.SelectedItem = null; 
-    }
+    
 
     private void Button_Clicked(object sender, EventArgs e)
     {
